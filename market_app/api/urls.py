97b44acbfe88c1d9b4ac4275145cf3fd9d2454b8
phischa <ph.git@mailbox.org>
2025-01-04 15:08:55 +0100

    request methods put and delete

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import first_view
+from .views import markets_view, market_single_view
 
 urlpatterns = [
-    path('', first_view),
+    path('', markets_view),
+    path('<int:pk>/', market_single_view)
 ]
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,14 +1,50 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import MarketSerializer
+from market_app.models import Market
 
 @api_view(['GET','POST'])
-def first_view(request):
+def markets_view(request):
+
     if request.method == 'GET':
-        return Response({"message": "Hello, World!"})
+        markets = Market.objects.all()
+        serializer = MarketSerializer(markets, many=True)
+        return Response(serializer.data)
+    
     if request.method == 'POST':
-        try:
+        serializer = MarketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+    """ try:
             msg = request.data['message']
             return Response({"your_message": msg}, status=status.HTTP_201_CREATED)
         except:
-            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST) """
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def market_single_view(request, pk):
+    
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)
