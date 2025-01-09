1ff08dec09a025b7e2adec56cfd702710d34e14a
phischa <ph.git@mailbox.org>
2025-01-09 12:19:51 +0100

    admin.py config

--- market_app/admin.py
+++ market_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Market, Seller, Product
 
 # Register your models here.
+
+admin.site.register(Market)
+admin.site.register(Seller)
+admin.site.register(Product)
